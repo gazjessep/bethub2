08342c883a467b83cd83b2fa039c678d32db5467
Gaz Jessep <gaz.jessep@gmail.com>
2016-04-08 17:10:30 +1200

    Updated crawler, should be operational

--- web/crawler.php
+++ web/crawler.php
@@ -9,14 +9,10 @@
 		$curl_scraped_page = curl_exec($curl_handle);
 		curl_close($curl_handle);
 
-	//	echo $curl_scraped_page;
-
 		//	use DOM to manipulate HTML
 		$dom = new DOMDocument();
 		//	@ suppresses errors from invalid HTML
 		@$dom->loadHTML($curl_scraped_page);
-		//	select results table
-		//	$results_table = $dom->getElementById('leagueresults_tbody');
 		
 		//
 		$xpath = new DOMXPath($dom);
@@ -28,52 +24,33 @@
 		if (!is_null($rows)) {
 			//	iterate through tr's
 			foreach ($rows as $row) {
-				//	var_dump( $row->nodeValue );
-				//	outputs text content of whole tr, as expected
-
 				//	team names
 				//	$teams = $xpath->query("td[1]/a", $row)->nodeValue;
-				$teams = $xpath->query("td[1]/a", $row);
-					var_dump($teams);
-					/* outputs something like
-					object(DOMNodeList)#857 (1) {
-					  ["length"]=>
-					  int(1)
-					} */
-					var_dump($teams->nodeValue);	//	outputs NULL
-
+				$teams = $xpath->query("td[1]/a", $row)->item(0)->nodeValue;
 				list($team_1, $team_2) = explode(" - ", $teams);
 				//	score
-				$score = $xpath->query("td[2]/a", $row)->nodeValue;
+				$score = $xpath->query("td[2]/a", $row)->item(0)->nodeValue;
 				list($goals_1, $goals_2) = explode(":", $score);
 				//	date
-				$date  = $xpath->query("td[6]", $row)->nodeValue;
-	
-	
+				$date  = $xpath->query("td[6]", $row)->item(0)->nodeValue;
+
 				$game = [	
-					'hteam' => $team_1,
-					'ateam' => $team_2,
-					'goals_ht' => $goals_1,
-					'goals_at' => $goals_2,
-					//	probably don't need to set timezone when this is non-null
-					'game_date' => new DateTime( $date, new DateTimeZone('UTC') )	
+					'hteam'		=> $team_1,
+					'ateam'		=> $team_2,
+					'goals_ht'	=> $goals_1,
+					'goals_at'	=> $goals_2,
+					//	not sure if this matter so long as they're all the same...
+					'game_date'	=> new DateTime( $date, new DateTimeZone('UTC') )	
 				];
-	
-				
-				//	var_dump($teams);
 
 				array_push($games, $game);
 
 
 			}	//	end foreach
 			
-	//		var_dump($games);
-			
 		}	//	end if
-
-		
-		var_dump($games[count($games)-1]);
-	}
+		return $games;
+	}	//	end crawlUrl
 	
 	crawlUrl("http://www.betexplorer.com/soccer/england/premier-league-2013-2014/results/");
 
